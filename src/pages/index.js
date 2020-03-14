04465ea44e3fd4603cc2797cf8c2cf38e4e61875
Jakub Ziebikiewicz <cactoo@users.noreply.github.com>
2020-03-14 14:00:18 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,54 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const VARS = [
+  "NODE_ENV",
+  "GATSBY_NODE_ENV",
+
+  // BUILD METADATA
+  "NETLIFY",
+  "BUILD_ID",
+  "CONTEXT",
+
+  // GIT METADATA
+  "REPOSITORY_URL",
+  "BRANCH",
+  "HEAD",
+  "COMMIT_REF",
+  "CACHED_COMMIT_REF",
+  "PULL_REQUEST",
+  "REVIEW_ID",
+
+  // DEPLOY URLS
+  "URL",
+  "DEPLOY_URL",
+  "DEPLOY_PRIME_URL",
+  "DEPLOY_ID",
+]
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <section style={{ display: "flex", justifyContent: "space-between" }}>
+    <div>
+      <h2>ENV VARIABLES:</h2>
+      <ul>
+        {VARS.map(v => (
+          <li key={v}>
+            {v}: <code>{process.env[v]}</code>
+          </li>
+        ))}
+      </ul>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <div>
+      <h2>GATSBY_ PREFIXED VARIABLES:</h2>
+      <ul>
+        {VARS.map(v => (
+          <li key={v}>
+            {v}: <code>{process.env[`GATSBY_${v}`]}</code>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </section>
 )
 
 export default IndexPage
